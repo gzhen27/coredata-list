04df5b857c6391e2bbca25ca1a03d37009483b79
Gray <grayzprogrammer@gmail.com>
2023-06-15 18:53:34 -0500

    Initial Commit

---
+++ CoreDataList/CoreDataListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataListApp.swift
+//  CoreDataList
+//
+//  Created by G Zhen on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
