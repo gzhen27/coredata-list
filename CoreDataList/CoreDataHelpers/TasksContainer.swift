78ca68a80a6ae9d83a003bab2f86187b896ed122
Gray <grayzprogrammer@gmail.com>
2023-06-20 19:00:22 -0500

    add mock data for preview mode

--- CoreDataList/CoreDataHelpers/TasksContainer.swift
+++ CoreDataList/CoreDataHelpers/TasksContainer.swift
@@ -10,10 +10,19 @@ import CoreData
 class TasksContainer {
     let persistentContainer: NSPersistentContainer
     
-    init() {
+    init(devMode: Bool = false) {
         persistentContainer = NSPersistentContainer(name: "Tasks")
+        
+        if devMode {
+            persistentContainer.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
         persistentContainer.loadPersistentStores { _, _ in
             // handling errors
+        }
+        
+        if devMode {
+            TasksContainer.addMockData(moc: persistentContainer.viewContext)
         }
     }
 }
---
+++ CoreDataList/Preview Content/TasksMockData.swift
@@ -0,0 +1,31 @@
+//
+//  TasksMockData.swift
+//  CoreDataList
+//
+//  Created by G Zhen on 6/20/23.
+//
+
+import CoreData
+
+extension TasksContainer {
+    static var previewContext: NSManagedObjectContext {
+        let persistentContainer = NSPersistentContainer(name: "Tasks")
+        persistentContainer.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        persistentContainer.loadPersistentStores { _, _ in }
+        addMockData(moc: persistentContainer.viewContext)
+        return persistentContainer.viewContext
+    }
+    
+    static func addMockData(moc: NSManagedObjectContext) {
+        let task1 = TaskEntity(context: moc)
+        task1.name = "First Task"
+        
+        let task2 = TaskEntity(context: moc)
+        task2.name = "Second Task"
+    
+        let task3 = TaskEntity(context: moc)
+        task3.name = "Third Task"
+        
+        try? moc.save()
+    }
+}
