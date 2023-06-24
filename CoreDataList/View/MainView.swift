b5d74f62e0a7d7e2e79aa05098acc55d1cfd6193
Gray <grayzprogrammer@gmail.com>
2023-06-24 17:42:13 -0500

    add filter functionality to the FetchRequest

--- CoreDataList/View/MainView.swift
+++ CoreDataList/View/MainView.swift
@@ -17,30 +17,34 @@ struct MainView: View {
     @State
     private var showTaskCreateView = false
     
+    @State
+    private var searchText = ""
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                List(tasks) { task in
-                    HStack {
-                        Image(systemName: task.isLike ? "heart.fill" : "heart")
-                            .font(.largeTitle)
-                            .foregroundColor(task.isLike ? .accentColor : Color("BlackandWhite"))
-                            .animation(.easeInOut, value: task.isLike)
-                            .onTapGesture {
-                                task.isLike.toggle()
-                                try? managedObjectContext.save()
-                            }
-                        VStack(alignment: .leading) {
-                            Text(task.getName)
-                                .font(.title2)
-                            Text("created at \(task.getCreatedAt)")
-                                .font(.footnote)
+            List(tasks) { task in
+                HStack {
+                    Image(systemName: task.isLike ? "heart.fill" : "heart")
+                        .font(.largeTitle)
+                        .foregroundColor(task.isLike ? .accentColor : Color("BlackandWhite"))
+                        .animation(.easeInOut, value: task.isLike)
+                        .onTapGesture {
+                            task.isLike.toggle()
+                            try? managedObjectContext.save()
                         }
+                    VStack(alignment: .leading) {
+                        Text(task.getName)
+                            .font(.title2)
+                        Text("created at \(task.getCreatedAt)")
+                            .font(.footnote)
                     }
                 }
             }
             .navigationTitle("Tasks")
-            .navigationBarTitleDisplayMode(.inline)
+            .searchable(text: $searchText)
+            .onChange(of: searchText) { text in
+                tasks.nsPredicate = text.isEmpty ? nil : NSPredicate(format: "name CONTAINS %@", text)
+            }
             .toolbar {
                 toolbarItems
             }
@@ -58,7 +62,6 @@ struct MainView: View {
                 Image(systemName: "plus")
                     .font(.title3)
             }
-
         }
     }
 }
